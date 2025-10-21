12c88a3617f260dd7294090888d000d00b9c1200
amirrajj <amiramraja@gmail.com>
2025-10-21 16:27:07 +0330

    prod docker files added and necessary change for task service

--- client/src/services/taskService.js
+++ client/src/services/taskService.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL;
+// const API_BASE_URL = import.meta.env.VITE_API_URL;
+const API_BASE_URL = '/api'; // for prod with nginx , nginx will proxy to backend
 
 class TaskService {
   async getAllTasks() {

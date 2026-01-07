13794bd2591b28d4d82f9d63d54bccf838654412
amirrajj <amiramraja@gmail.com>
2026-01-07 16:27:33 +0330

    push new changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const name = "amirhosein"
 
   // Fetch tasks on component mount
   useEffect(() => {
